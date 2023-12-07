ea6f57e5ff2240bbe2ac5990774f8ed30fe853f0
Thang X. Vu <thang@coongcrafts.io>
2023-12-07 20:42:30 +0700

    chore: fix typo

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,11 +6,11 @@ const env = {
   isProd,
   isDev: !isProd,
   motherAddress: {
-    dev: import.meta.env.VITE_DEV_MOTHERADRESS,
-    testnetRococo: import.meta.env.VITE_TESTNET_ROCOCO_MOTHERADRESS,
-    testnetAlephZero: import.meta.env.VITE_TESTNET_ALEPHZERO_MOTHERADRESS,
-    testnetShibuya: import.meta.env.VITE_TESTNET_SHIBUYA_MOTHERADRESS,
-    testnetPhala: import.meta.env.VITE_TESTNET_PHALA_MOTHERADRESS,
+    dev: import.meta.env.VITE_DEV_MOTHERADDRESS,
+    testnetRococo: import.meta.env.VITE_TESTNET_ROCOCO_MOTHERADDRESS,
+    testnetAlephZero: import.meta.env.VITE_TESTNET_ALEPHZERO_MOTHERADDRESS,
+    testnetShibuya: import.meta.env.VITE_TESTNET_SHIBUYA_MOTHERADDRESS,
+    testnetPhala: import.meta.env.VITE_TESTNET_PHALA_MOTHERADDRESS,
   },
   defaultChainId: isProd ? RococoContractsTestnet.id : Development.id,
 };
