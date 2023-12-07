d0cb4ce5b3484c5973f0846870cb120141ff2bd5
Thang X. Vu <thang@coongcrafts.io>
2023-12-07 15:41:26 +0700

    tweak: check motheraddress

--- src/hooks/contracts/useMotherContract.ts
+++ src/hooks/contracts/useMotherContract.ts
@@ -6,5 +6,9 @@ import { ChainId } from 'useink/chains';
 export default function useMotherContract(chainId: ChainId) {
   const { motherAddress } = findNetwork(chainId);
 
+  if (!motherAddress) {
+    throw new Error('Missing MotherAddress!');
+  }
+
   return useContract(motherAddress, motherspace, chainId);
 }
